65923424964508234db094cb4299fabcde4b2fc8
kai-git-stuff <kai.habermann@gmx.net>
2022-02-01 17:42:37 +0100

    first proper angular distribution

--- tests/mytest.py
+++ tests/mytest.py
@@ -19,30 +19,7 @@ mc = 493.677 # K-
 md = 5912.19 
 phsp = DalitzPhaseSpace(ma,mb,mc,md) 
 
-
 smp = PhaseSpaceSample(phsp,phsp.unfiltered_sample(10000,maximum=1.0))
-
-
-
-
-
-@atfi.function
-def fac(n):
-    if (n< 2):
-        return 1
-    return atfi.reduce_prod(atfi.range(1,n+1))
-
-@atfi.function
-def d(J_x,lmbda_c,lmbda_D,theta):
-    s = atfi.range(max(-(lmbda_c-lmbda_D),0),max(lmbda_D + J_x, J_x - lmbda_c))
-    theta_2 = theta/2.
-    return (
-        sqrt(fac(J_x + lmbda_c) *fac(J_x - lmbda_c) * fac(J_x + lmbda_D) *  fac(J_x - lmbda_D)) * 
-    atfi.sum((-1)**s /(fac(J_x + lmbda_D - s) * fac(s) * fac(lmbda_c - lmbda_D + s) * fac(J_x - lmbda_c - s)) * 
-    atfi.cos(theta_2)**(2*J_x +lmbda_D - lmbda_c - 2*s) * 
-    atfi.sin(theta_2)**(lmbda_c - lmbda_D - 2*s))
-    )
-
 
 sgma1 = phsp.m2ab(smp)
 sgma2 = phsp.m2ac(smp)
@@ -64,47 +41,49 @@ def calculate():
 def winkel_verteillung(phi,theta,J,m1,m2):
     return wigner_capital_d(phi,theta,0,J,m1,m2)
 
-def winkel_verteillung_multiple_channels(phi,theta,J,s1,s2,l1,l2,bls):
+def angular_distribution_multiple_channels(phi,theta,J,s1,s2,l1,l2,bls):
     return atfi.cast_complex(helicity_couplings_from_ls(J,s1,s2,l1,l2,bls)) * atfi.cast_complex(wigner_capital_d(phi,theta,0,J,l1,l2))
 
 phi, theta = np.meshgrid(np.linspace(0,2*np.pi,100),np.linspace(0,np.pi,100))
-#print(phi)
+phi, theta = tf.meshgrid(np.linspace(0,2*np.pi,100),np.linspace(0,np.pi,100))#print(phi)
 #ampl = winkel_verteillung_multiple_channels(phi,theta,sp.SPIN_2,sp.SPIN_HALF,sp.SPIN_HALF,1,1,{(sp.SPIN_1,sp.SPIN_1):1,(0,sp.SPIN_1):1})
 
 
+def helicity_options(J,s1,s2):
+    options = []
+    for m1 in range(-s1,s1+1,2):
+        for m2 in range(-s2,s2+1,2):
+            if m1+m2 < J:
+                options.append((m1,m2))
+    return options
+
+def coupling_options(J,s1,s2):
+    bls = {}
+    s_max,s_min = s1+s2, abs(s1-s2)
+    for s in range(s_min,s_max+1,2):
+        for l in range(0,J+s+1,2):
+            if J <= l+s and J >= abs(l-s):
+                bls[(l,s)] = 1
+    return bls
+
+
 J = sp.SPIN_2
-s1 = sp.SPIN_HALF
-s2 = sp.SPIN_HALF
-l1 = 1
-l2 = 1
+s1 = sp.SPIN_1
+s2 = sp.SPIN_1
+
 bls = {(sp.SPIN_1,sp.SPIN_1):1,(sp.SPIN_2,sp.SPIN_0):1}
 
-ampl = winkel_verteillung_multiple_channels(phi,theta,J,s1,s2,l1,l2,bls)
-
-
-
-
-
-
-
-
-
-
+options = helicity_options(J,s1,s2)
+l1,l2 = options.pop(0)
+bls = coupling_options(J,s1,s2)
+ampl = angular_distribution_multiple_channels(phi,theta,J,s1,s2,l1,l2,bls)
+print(bls)
+print(options)
+for l1,l2 in options:
+    ampl += angular_distribution_multiple_channels(phi,theta,J,s1,s2,l1,l2,bls)
 
 #print(ampl)
 plt.imshow(abs(ampl), extent=[0,2*np.pi,0,np.pi], origin='lower')
 plt.show()
 
 
-@atfi.function
-def H(lmbda_i,lmbda_f):
-    return atfi.complex(1,0)
-
-@atfi.function
-def R(m,M0,gamma):
-    pass
-
-
-@atfi.function
-def M(lmbda_i,lmbda_f,j):
-    pass
